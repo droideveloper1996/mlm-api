f099eea75654c95a3b3c8bca68af35428100f6e1
System Administrator <root@Abhisheks-MacBook-Pro-4.local>
2019-09-23 14:54:20 +0530

    bcrypt changes 4

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports.bcrypt = bcrypt;
 
 
 dotenv.config();
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000
 var app = express();
 
 app.use((req, res, next) => {

9014746167058d8fbf386af1d8e290b97dfb9d1d
System Administrator <root@Abhisheks-MacBook-Pro-4.local>
2019-09-28 01:07:07 +0530

    27 Sept 2019 1st commit

--- routes/office.js
+++ routes/office.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { verifyToken } = require('../validator');
 const { loginValidation, registerValidation } = require('../joi-schema/joi-validator');
 const OfficeUser = require('../mongoose-model/mongoose-office');
+const { studentRegistrationSchema } = require('../mongoose-model/mongoose-student');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('../index.js').bcrypt;
 
@@ -64,6 +65,134 @@ router.post('/office-register', async (req, res) => {
 
 
 })
+
+router.get('/getAllStudents', verifyToken, async (req, res) => {
+    console.log(req)
+    try {
+        const stud = await studentRegistrationSchema.find({});
+        if (!stud) {
+            return res.status(400).json({ message: "No Student Found", status: 400 })
+        }
+        else {
+            return res.status(400).json(stud)
+        }
+    }
+    catch (error) {
+        return res.status(400).json({ message: error.message, status: 400 })
+    }
+});
+
+router.get('/getStudentById', verifyToken, async (req, res) => {
+    try {
+        const id = req.body.studentId;
+        if (id == null || id == '') return res.status(400).json({ message: "Student Id/Scholar Number must be provided", status: 400 })
+        const student = await studentRegistrationSchema.findOne({ studentID: id });
+        if (!students) return res.status(400).json({ message: "No Such Student Exists", status: 400 })
+        else return res.status(200).json(student)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+
+router.get('/getStudentByBoard', verifyToken, async (req, res) => {
+    try {
+        const boardType = req.body.boardType;
+        if (boardType == null || boardType == '') return res.status(400).json({ message: "Specify Board", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ boardType: boardType });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+
+router.get('/getStudentByMedium', verifyToken, async (req, res) => {
+    try {
+        const medium = req.body.medium;
+        if (medium == null || medium == '') return res.status(400).json({ message: "Specify Medium", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ medium: medium });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+
+router.get('/getStudentByWing', verifyToken, async (req, res) => {
+    try {
+        const wingType = req.body.wingType;
+        if (wingType == null || wingType == '') return res.status(400).json({ message: "Specify Wing", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ wingType: wingType });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+
+router.get('/getStudentByGender', verifyToken, async (req, res) => {
+    try {
+        const gender = req.body.gender;
+        if (gender == null || gender == '') return res.status(400).json({ message: "Specify Gender", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ gender: gender });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+router.get('/getStudentByConveyance', verifyToken, async (req, res) => {
+
+    try {
+        const conveyance = req.body.conveyance;
+        if (conveyance == null || conveyance == '') return res.status(400).json({ message: "Specify Conveyance", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ conveyance: conveyance });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+router.get('/getStudentByDisability', verifyToken, async (req, res) => {
+
+    try {
+        const disability = req.body.disability;
+        if (disability == null || disability == '') return res.status(400).json({ message: "Specify Disability", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ disability: disability });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
+router.get('/getStudentByAdmissionType', verifyToken, async (req, res) => {
+
+    try {
+        const admissionType = req.body.admissionType;
+        if (admissionType == null || admissionType == '') return res.status(400).json({ message: "Specify Admission Type", status: 400 })
+        const students = await studentRegistrationSchema.findOne({ admissionType: admissionType });
+        if (!students) return res.status(400).json({ message: "No Results Found ", status: 400 })
+        else return res.status(200).json(students)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+
+});
 
 
 module.exports.office = router;
--- validator.js
+++ validator.js
@@ -1,19 +1,13 @@
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 module.exports.verifyToken = (req, res, next) => {
     const token = req.header('auth-token');
     if (!token)
         return res.status(400).json({ message: 'Access Denied', status: 400 });
 
     try {
-        var info = jwt.verify(token, process.env.SECRET_KEY, (error, data));
-        if (error) {
-
-            return res.status(400).json({ status: 400, message: error.message });
-        } else {
-            req.user = data;
-            next();
-        }
+        var info = jwt.verify(token, process.env.SECRET_KEY);
+        if (!info) { return res.status(400).json({ status: 400, message: error.message }); }
+        else { req.user = info; next(); }
     }
     catch (e) {
         return res.status(400).json({ message: e.message, status: 400 });
